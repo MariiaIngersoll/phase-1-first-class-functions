484c420f25d95bb262d420594092d6d1acefd893
+ <+>
2023-02-24 20:19:05 -0500

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(java) {
+    return java();
+};
+
+function returnsANamedFunction() {
+    return function menu(cheese) {
+        return `Meals`
+    } 
+};
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        return `Hoy`
+    }
+
+}
